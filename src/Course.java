dc551b7668fd623b9d1e1fdd6ad242a1d0d63728
Matthew <mwong447@uwo.ca>
2017-02-15 23:15:46 -0500

    Tutorial

---
+++ src/Course.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Matthew on 2017-02-15.
+ */
+public class Course {
+
+    String cname;
+    String description;
+
+    public Course(String cname, String description){
+
+        this.cname = cname;
+        this.description = description;
+
+    }
+
+}
